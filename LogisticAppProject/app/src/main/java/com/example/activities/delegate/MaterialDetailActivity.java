05db28451879653259248d5b9eac157102f45d19
Eduardo Montenegro <eduardo-thrash@hotmail.com>
2020-05-31 00:38:22 -0500

    MaterialDetail migration

---
+++ LogisticAppProject/app/src/main/java/com/example/activities/delegate/MaterialDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.activities.delegate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.logisticappproject.R;
+
+public class MaterialDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_detail);
+    }
+}
--- LogisticAppProject/app/src/main/java/com/example/logisticappproject/MainActivity.java
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.logisticappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.activities.delegate.MaterialDetailActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        Intent launcherActivity = new Intent(this, MaterialDetailActivity.class);
+        startActivity(launcherActivity);
     }
 }
