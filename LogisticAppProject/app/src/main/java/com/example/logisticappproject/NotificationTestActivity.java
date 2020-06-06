07631304f9d7bf5e0172cb5365467ed98f1eb5cc
Eduardo Montenegro <eduardo-thrash@hotmail.com>
2020-06-06 00:23:35 -0500

    Creation of page for RF9

---
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/NotificationTestActivity.java
@@ -0,0 +1,14 @@
+package com.example.logisticappproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_test);
+    }
+}
