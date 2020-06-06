58a07723fe1b402640b6ca83db0d3d36d69b1742
rjrubior <37877167+rjrubior@users.noreply.github.com>
2020-06-05 20:50:06 -0500

    Creación página requerimiento RF4

---
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/DepartureRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.logisticappproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepartureRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_departure_register);
+    }
+}
