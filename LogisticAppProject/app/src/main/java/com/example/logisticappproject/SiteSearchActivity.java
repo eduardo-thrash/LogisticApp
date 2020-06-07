12c76e8a6a3a3d400a0729e7936d3166fd883088
ingedwindiaz <64512283+ingedwindiaz@users.noreply.github.com>
2020-06-07 13:22:26 -0400

    RF17 publicación Inicial

---
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/SiteSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.logisticappproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SiteSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_site_search);
+    }
+}
