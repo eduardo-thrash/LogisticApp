82457f17d15f3e81f560613ee8b757bebcee77ad
Eduardo Montenegro <eduardo-thrash@hotmail.com>
2020-06-07 17:11:10 -0500

    Creation page for HU MaterialDetail, TestDetail, TestScan and TestSearch

---
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/MaterialDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.logisticappproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaterialDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_detail);
+    }
+}
---
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/TestSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.logisticappproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_search);
+    }
+}
