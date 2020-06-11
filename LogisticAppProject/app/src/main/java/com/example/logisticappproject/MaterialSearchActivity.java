7741a2becac22cbca967525de9342b81fdf282a5
rjrubior <37877167+rjrubior@users.noreply.github.com>
2020-06-10 19:59:50 -0500

    RF6_layoutsConnection

--- LogisticAppProject/app/src/main/java/com/example/logisticappproject/MaterialSearchActivity.java
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/MaterialSearchActivity.java
@@ -2,7 +2,9 @@ package com.example.logisticappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MaterialSearchActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MaterialSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_search);
+    }
+
+    public void MaterialDetailActivity (View view){
+        Intent MaterialDetailActivity = new Intent(this, MaterialDetailActivity.class);
+        startActivity(MaterialDetailActivity);
+
     }
 }
