47df75ed93046dfbcd4a890d4a49db36d6f852f2
rjrubior <37877167+rjrubior@users.noreply.github.com>
2020-06-10 20:53:03 -0500

    RF19_LayoutsConnection

--- LogisticAppProject/app/src/main/java/com/example/logisticappproject/RoomResumeActivity.java
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/RoomResumeActivity.java
@@ -2,7 +2,9 @@ package com.example.logisticappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RoomResumeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RoomResumeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_resume);
+    }
+    public void SiteNotificationDetailByRoomActivity (View view){
+        Intent SiteNotificationDetailByRoomActivity = new Intent(this, SiteNotificationDetailByRoomActivity.class);
+        startActivity(SiteNotificationDetailByRoomActivity);
+
     }
 }
