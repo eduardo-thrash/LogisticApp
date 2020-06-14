0a6e6539b9c13901747a6ea0732f7c85fa7d8a07
sandracelis <47510682+sandracelis@users.noreply.github.com>
2020-06-13 21:05:32 -0500

    RF7 Layouts Connection
    RF7 Layouts Connection

--- LogisticAppProject/app/src/main/java/com/example/logisticappproject/ScanMaterialActivity.java
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/ScanMaterialActivity.java
@@ -11,4 +11,10 @@ public class ScanMaterialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_material);
     }
+
+    public void MaterialDetailActivity (View view){
+        Intent MaterialDetailActivity = new Intent(this, MaterialDetailActivity.class);
+        startActivity(MaterialDetailActivity);
+    }
+
 }
