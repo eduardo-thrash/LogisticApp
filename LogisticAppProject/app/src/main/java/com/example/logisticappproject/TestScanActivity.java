7335bacc50ce6796c7a24ec7425d903d21e39c81
sandracelis <47510682+sandracelis@users.noreply.github.com>
2020-06-13 21:42:14 -0500

    RF12 Layouts Connection
    RF12 Layouts Connection

--- LogisticAppProject/app/src/main/java/com/example/logisticappproject/TestScanActivity.java
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/TestScanActivity.java
@@ -2,7 +2,10 @@ package com.example.logisticappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class TestScanActivity extends AppCompatActivity {
 
@@ -11,4 +14,11 @@ public class TestScanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_scan);
     }
+	
+	public void TestDetailActivity (View view){
+        Intent TestDetailActivity = new Intent(this, TestDetailActivity.class);
+        startActivity(TestDetailActivity);
+
+    }
+	
 }
