bae7f363af99e471f832da84c804a63abd448382
sandracelis <47510682+sandracelis@users.noreply.github.com>
2020-06-13 21:54:52 -0500

    RF13 Layouts Connection
    RF13 Layouts Connection

--- LogisticAppProject/app/src/main/java/com/example/logisticappproject/TestDetailActivity.java
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/TestDetailActivity.java
@@ -2,7 +2,9 @@ package com.example.logisticappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TestDetailActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class TestDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_detail);
+    }
+	
+	 public void NotificationTestActivity (View view){
+        Intent NotificationTestActivity = new Intent(this, NotificationTestActivity.class);
+        startActivity(NotificationTestActivity);
+
     }
 }
