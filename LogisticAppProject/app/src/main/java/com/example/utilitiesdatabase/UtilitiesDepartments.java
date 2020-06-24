c856d3d49075749f996ad06f407974ac33bf4fb9
ingedwindiaz <64512283+ingedwindiaz@users.noreply.github.com>
2020-06-23 21:32:40 -0400

    Insert DB Jun 23
    Insert DB Jun 23

--- LogisticAppProject/app/src/main/java/com/example/utilitiesdatabase/UtilitiesDepartments.java
+++ LogisticAppProject/app/src/main/java/com/example/utilitiesdatabase/UtilitiesDepartments.java
@@ -17,15 +17,40 @@ public class UtilitiesDepartments {
         ArrayList<String> InsertDepartments;
         InsertDepartments = new ArrayList<>();
 
-        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Cundinamarca')");
+
         InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Antioquia')");
-        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Valle del cauca')");
-        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Nariño')");
-        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Bolivar')");
         InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Atlantico')");
-        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Leticia')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Amazonas')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Arauca')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Bogota')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Bolivar')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Boyaca')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Caldas')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Caqueta')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Cauca')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Casanare')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Cesar')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Cordoba')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Cundinamarca')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Choco')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Guainia')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Guaviare')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Huila')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('La Guajira')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Magdalena')");
         InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Meta')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Nariño')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Norte de Santander')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Putumayo')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Quindio')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Risaralda')");
         InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Santander')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Sucre')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Tolima')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Valle del Cauca')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Vaupes')");
+        InsertDepartments.add("INSERT INTO DEPARTMENTS (department_name) VALUES ('Vichada')");
+
 
         for (int i = 0; i<InsertDepartments.size();i++){
             db.execSQL(InsertDepartments.get(i));
