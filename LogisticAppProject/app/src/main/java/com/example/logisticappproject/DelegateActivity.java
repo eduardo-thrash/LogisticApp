9256797b85848b298c4e2dd0e38bd9a866dc0e01
LeidiBernal <64670527+LeidiBernal@users.noreply.github.com>
2020-07-02 21:24:22 -0400

    HU01_Login_Issue85_87_100_101
    HU01_Login_Issue85_87_100_101

--- LogisticAppProject/app/src/main/java/com/example/logisticappproject/DelegateActivity.java
+++ LogisticAppProject/app/src/main/java/com/example/logisticappproject/DelegateActivity.java
@@ -59,6 +59,7 @@ public class DelegateActivity extends AppCompatActivity {
 
         if(_sessionBusinessRules.DestroySession(conn)){
             Intent LauncherActivity = new Intent(this, com.example.logisticappproject.LoginActivity.class);
+            LauncherActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(LauncherActivity);
         }else{
             Toast.makeText(getApplicationContext(),"Error al cerrar sesión, intente de nuevo.",Toast.LENGTH_LONG).show();
